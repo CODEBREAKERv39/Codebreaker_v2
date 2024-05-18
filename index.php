ddda2563529e716aea3737e40d9b3ff4686b4124
CODEBREAKERv39 <169062899+CODEBREAKERv39@users.noreply.github.com>
2024-05-18 12:35:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Define the path to your Go file
+
+if(file_exists("bgmi")){
+echo "true";
+}else{
+echo "false";
+}
+
+// Define the command to run the Go file
+//$command = "go run $FilePath";
+
+// Execute the command and capture the output
+//$output = shell_exec($command);
+
+// Display the output
+//echo "<pre>$output</pre>";
+?>
